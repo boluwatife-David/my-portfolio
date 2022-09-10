6642e092b7f729a05749e9cd4cd2ab4fb44d538a
boluwatife-David <boldav20000309@gmail.com>
2022-09-10 07:48:06 +0100

    New solo project I guess

---
+++ src/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function HeaderRoot() {
+    return (
+        <div>
+             <img/>
+             <h1>NUMB</h1>
+             <p>I am NUMB.</p>
+             <div>
+                <p>This is the third line </p>
+                <botton className="white-btn"></botton>
+                <botton className="blue-btn"></botton>
+             </div>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import  ReactDOM from "react";
+import "../src/components/styles/style.css"
+import HeaderRoot from "./header"
+
+ReactDOM.render(<HeaderRoot />, document.getElementById("root"))
