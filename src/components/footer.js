c87a281f433cddaebf4a1942c1bacdc0c4c3532a
boluwatife-David <boldav20000309@gmail.com>
2022-09-11 08:57:55 +0100

    working on the props

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/style.css"
+import { FaGithubSquare } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter  } from "react-icons/fa";
+export default function FooterRoot () {
+    return (
+        <div className="footer">
+            <FaGithubSquare className="footer-icon"/>
+            <FaFacebookF className="footer-icon"/>
+            <FaInstagram className="footer-icon"/>
+            <FaTwitter className="footer-icon"/>
+        </div>
+    )
+}
