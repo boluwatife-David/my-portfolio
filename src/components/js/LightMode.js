c53d7f250851c6c29fdf0eb2539291b24e17a5c5
boluwatife-David <boldav20000309@gmail.com>
2022-09-18 12:10:01 +0100

    toggle working need to fix the styles

--- src/components/js/LightMode.js
+++ src/components/js/LightMode.js
@@ -4,9 +4,7 @@ import { FaCircle } from "react-icons/fa";
 
 export default function LightMode() {  
     return (
-        <div className="theme-btn">
-            <FaCircle className="lightmode-btn" title="light mode"/>
-        </div>
+       
      
     )
 }
