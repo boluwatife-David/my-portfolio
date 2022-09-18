51445a80c635d7ed3cd5b0bc99f08377505e4f60
boluwatife-David <boldav20000309@gmail.com>
2022-09-18 14:15:54 +0100

    working on my light mode button

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ContentRoot from "./components/js/content";
 import HeaderRoot from "./components/js/header";
-import LightMode from "./components/js/LightMode";
 import projectprops from "./components/js/projectprops";
 import FooterRoot from "./components/js/footer";
 export default function App () {
 const [lightTheme, setLightTheme] = React.useState(false)
+
 
     const projectprop = projectprops.map(myproject => {
       return <ContentRoot img={myproject.img} 
@@ -15,12 +15,15 @@ const [lightTheme, setLightTheme] = React.useState(false)
    })
 
     return (
-        <div className="" >
-            <HeaderRoot />
+        <div className={lightTheme ? "light-mode" : "body"} >
+            <nav className="theme-btn">
+                <button className="lightmode-btn" onClick={() => setLightTheme(prevTheme => !prevTheme)} >light</button>
+            </nav>
             {/* <div className="project-list">
                {projectprop}
             </div> */}
-            <div >
+            <div className="content" >
+            <HeaderRoot />
             <FooterRoot />
             </div>
             
--- src/components/js/header.js
+++ src/components/js/header.js
@@ -4,18 +4,17 @@ import { FaEnvelope } from "react-icons/fa";
 import { FaArrowRight  } from "react-icons/fa";
 import { FaRegMehRollingEyes  } from "react-icons/fa";
 import { FaRegGrinBeam  } from "react-icons/fa";
-import LightMode from "./LightMode"
 export default function HeaderRoot() {
     const welcomeUser = "HEYYY, WELCOME"
     const me = " I am NUMB."
+
     return (
         <div className="img-header">
-            <p><LightMode /></p>
-             <h1 className="header"><FaRegMehRollingEyes /></h1>
+             <h1 className="header"><FaRegMehRollingEyes className="emoji"/></h1>
              <h1 className="header"><span>{welcomeUser} {me}</span></h1>
              <p className="numbel"> <span className="lol">lol...</span></p>
              <div className="welcome">
-                <p className="para3">Your're Welcome. <FaRegGrinBeam /></p>
+                <p className="para3">Your're Welcome. <FaRegGrinBeam className="emoji"/></p>
                 <button className="white-btn" type="button" title="Hire me please"><FaEnvelope /> Email</button>
                 <button className="projects-btn" type="button"> Projects <FaArrowRight className="arrow"/> </button>
              </div>
