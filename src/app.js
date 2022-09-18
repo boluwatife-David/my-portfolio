10fe666817d71971b86cd32b7361c217d1c05d76
boluwatife-David <boldav20000309@gmail.com>
2022-09-18 11:35:37 +0100

    omoooo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ContentRoot from "./components/js/content";
 import HeaderRoot from "./components/js/header";
-
+import LightMode from "./components/js/LightMode";
 import projectprops from "./components/js/projectprops";
 import FooterRoot from "./components/js/footer";
 export default function App () {
+const [lightTheme, setLightTheme] = React.useState(false)
+
     const projectprop = projectprops.map(myproject => {
       return <ContentRoot img={myproject.img} 
       name={myproject.name}
@@ -13,12 +15,15 @@ export default function App () {
    })
 
     return (
-        <div className="portfolio">
+        <div className="" >
             <HeaderRoot />
             {/* <div className="project-list">
                {projectprop}
             </div> */}
+            <div >
             <FooterRoot />
+            </div>
+            
         </div>
 
     )
